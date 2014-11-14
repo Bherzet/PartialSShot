ceee135b0b8a01e680bd611033039a250b014d4f
Tomáš Zíma <tomas.zima.66@gmail.com>
2014-11-14 23:52:28 +0100

    Created skeleton of the class for running uploads in a new thread.

---
+++ PartialSShot/src/cz/tzima/partialsshot/uploader/GeneralUploader.java
@@ -0,0 +1,10 @@
+package cz.tzima.partialsshot.uploader;
+
+/**
+ * Runs the content publisher in its own thread, therefore preventing freezing
+ * of the main thread.
+ * 
+ * @author Tomas Zima
+ */
+public class GeneralUploader extends Thread {
+}
